1b1b25f7f2a72cddeec38dc4d787b001dcb253d3
Joaquín331 <joaquinpintorrr@gmail.com>
2022-12-13 12:35:18 -0400

    crud angular joaquim

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyAahGalBVa_DmFO07UMPAhhBLRCWVEwaHI",
+  authDomain: "crud-angular-firebase-cu-4d59e.firebaseapp.com",
+  projectId: "crud-angular-firebase-cu-4d59e",
+  storageBucket: "crud-angular-firebase-cu-4d59e.appspot.com",
+  messagingSenderId: "191935597896",
+  appId: "1:191935597896:web:523b4db32841767242f77d"
+  }
+};
